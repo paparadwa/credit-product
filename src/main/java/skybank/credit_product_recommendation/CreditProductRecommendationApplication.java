d68ba2f26bf7398273ab8416b40bb6dc30db61f3
paparadwa <timoshkov_a@bk.ru>
2026-05-30 16:49:36 +0300

    initial commit

---
+++ src/main/java/skybank/credit_product_recommendation/CreditProductRecommendationApplication.java
@@ -0,0 +1,13 @@
+package skybank.credit_product_recommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditProductRecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditProductRecommendationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/skybank/credit_product_recommendation/CreditProductRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package skybank.credit_product_recommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditProductRecommendationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
